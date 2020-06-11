29c2ba0dc239065ab745a8086a056213b87fbfef
Mitrasish Mukherjee <mmitrasish97@gmail.com>
2020-06-11 22:46:56 +0530

    WIP: Try to fix icon not showing in test flight

--- android/app/src/main/java/com/bluzelleinstantnotary/MainApplication.java
+++ android/app/src/main/java/com/bluzelleinstantnotary/MainApplication.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.faizal.OtpVerify.RNOtpVerifyPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
