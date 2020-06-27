64ea8323562deaeb328d956834bd626a4e2df0ee
Mitrasish Mukherjee <mmitrasish97@gmail.com>
2020-06-28 03:41:53 +0530

    Fix header error

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 export const MAGIC_NUMBER = "9999999999";
 export const MAGIC_NUMBER_COUNTRY_CODE = "+1";
 export const MAGIC_NUMBER_AUTH = true;
-export const ROOT_URL = "https://bluzelle-notary-backend.herokuapp.com";
+export const ROOT_URL =
+  "http://ec2-100-24-255-158.compute-1.amazonaws.com:5000";
--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000; // default port to listen
 function haltOnTimedOut(req: any, res: any, next: any) {
   if (!req.timedout) next();
 }
-// app.use(timeout("5s"));
+app.use(timeout("5s"));
 app.use(bodyParser.json());
 app.use(haltOnTimedOut);
 
@@ -33,18 +33,6 @@ app.use(haltOnTimedOut);
 
 app.use(cors());
 app.use(haltOnTimedOut);
-
-app.use("/uploadNotary/file", (req, res, next) => {
-  req.setTimeout(5 * 60 * 1000); // No need to offset
-
-  req.socket.removeAllListeners("timeout"); // This is the work around
-  req.socket.once("timeout", () => {
-    req.timedout = true;
-    res.status(504).send("Timeout");
-  });
-
-  next();
-});
 
 router(app);
 
